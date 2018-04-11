9b50d4be7721c5c3af9b475cee10a9f2dbf05366
Adam Idone <ajidone@gmail.com>
2018-04-11 08:33:03 -0700

    online icons added

--- src/App.js
+++ src/App.js
@@ -4,24 +4,29 @@ import './App.css';
 import Landing from './components/Landing';
 import Library from './components/Library';
 import Album from './components/Album';
+import { Grid, Row, Col, Navbar } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header>
-          <nav>
-           <Link to='/'>Landing</Link>
-           <Link to='/library'>Library</Link>
-           <Link to='/album'>Album</Link>
-          </nav>
-          <h1>Bloc Jams</h1>
-        </header>
-        <main>
-        <Route exact path="/" component={Landing} />
-        <Route path="/library" component={Library} />
-        <Route path="/album/:slug" component={Album} />
-        </main>
+        <Grid fluid>
+        <Col xs={12} md={8}>
+          <header>
+            <nav>
+             <Link to='/'>Landing</Link>
+             <Link to='/library'>Library</Link>
+             <Link to='/album'>Album</Link>
+            </nav>
+            <h1>Bloc Jams</h1>
+          </header>
+          </Col>
+          <main>
+          <Route exact path="/" component={Landing} />
+          <Route path="/library" component={Library} />
+          <Route path="/album/:slug" component={Album} />
+          </main>
+        </Grid>
       </div>
     );
   }
